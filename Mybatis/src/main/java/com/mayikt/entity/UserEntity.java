7fb362415e1ca5255b95c0494b22080aad6db555
myck <zhangwei123>
2019-06-12 19:58:41 +0800

    first commit

---
+++ Mybatis/src/main/java/com/mayikt/entity/UserEntity.java
@@ -0,0 +1,13 @@
+package com.mayikt.entity;
+
+import lombok.Data;
+
+@Data
+public class UserEntity {
+
+    private int id;
+
+    private String name;
+
+    private int age;
+}
---
+++ Mybatis/src/main/java/com/mayikt/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.mayikt.mapper;
+
+import com.mayikt.entity.UserEntity;
+
+public interface UserMapper {
+
+    public UserEntity getUser(int id);
+}
